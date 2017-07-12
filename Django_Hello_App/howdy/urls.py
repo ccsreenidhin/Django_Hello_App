41e2a05ed792429b98d679ab106d3c5ef25f67a8
c c sreenidhin <ccsreenidhin@gmail.com>
2017-07-12 23:31:00 +0530

    initial commit

---
+++ Django_Hello_App/howdy/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+
+from howdy import views
+
+
+urlpatterns = [
+
+  url(r'^$', views.HomePageView.as_view()),
+  url(r'^about/$', views.AboutPageView.as_view()), # Add this /about/ route
+
+
+  ]
+
+
---
+++ onecommit.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+git init
+git add .
+read -p "Enter the commit date : " date
+echo "commiting on, $date"
+git commit -m "initial commit" --date $date
+read -p "Enter the remote repo : " repo
+echo "pushing to repo, $repo"
+git add remote origin $repo
+git push -u origin master
+send "ccsreenidhin\r"
+send "#Redfvc#890#\r"
+
